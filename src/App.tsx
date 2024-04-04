62cc77e9dcb1b19872cf442d09664eb78f94c5d8
Andrii Shevchuk <supamercron@gmail.com>
2024-04-03 20:51:38 -0400

    add css modules

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './index.scss';
+import { Counter } from "./components/Counter";
+
+const App = () => {
+    return (
+        <div className="app">
+            some text
+            <Counter />
+        </div>
+    );
+}
+ 
+
+
+export default App;

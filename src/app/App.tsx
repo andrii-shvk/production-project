ec9fecea4e5f6799370fe7eb8ccd92a748c75147
Andrii Shevchuk <supamercron@gmail.com>
2024-04-04 08:35:11 -0400

    add architecture in project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,14 @@
 import './styles/index.scss';
 import { Route, Routes } from 'react-router-dom';
 import { Link } from 'react-router-dom';
-import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { Suspense } from 'react';
-import { useTheme } from './theme/useTheme';
-import { classNames } from './helpers/classNames/classNames';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { useTheme } from './providers/ThemeProvider';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 
 const App = () => {
     const {theme, toggleTheme} = useTheme();
-    const bool = true;
 
     return (
         <div className={classNames('app', {hovered: true, selected: true}, [theme, 'cls2', 'cls3'])}>
@@ -18,8 +17,8 @@ const App = () => {
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />} />
-                    <Route path={'/'} element={<MainPageAsync />} />
+                    <Route path={'/about'} element={<AboutPage />} />
+                    <Route path={'/'} element={<MainPage />} />
                 </Routes>
             </Suspense>
         </div>

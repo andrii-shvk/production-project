1d7b41de24f973f4973efa89f2c4d264556cc0ae
Andrii Shevchuk <supamercron@gmail.com>
2024-04-03 22:45:59 -0400

    add ThemeContext, ThemeProvider, useTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import './index.scss';
+import './styles/index.scss';
 import { Route, Routes } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { Suspense } from 'react';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
